860d66bef6838a81215e908b246350fdb655350a
Maximilian Gaedig <mg@maximiliangaedig.com>
2026-05-11 02:00:35 +0200

    Revert "sdm845: kernel: 6.16.7 -> 6.19.0-rc6-next-20260119"
    This reverts commit 8b510c23278e49df429c0435327b5710c5e909ea.

--- devices/families/sdm845-mainline/kernel/default.nix
+++ devices/families/sdm845-mainline/kernel/default.nix
@@ -6,19 +6,15 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "6.19.0-rc6-next-20260119";
+  version = "6.16.7";
   configfile = ./config.aarch64;
 
   src = fetchFromGitLab {
-    owner = "sdm845";
-    repo = "sdm845-next";
-    rev = "sdm845-next";
-    hash = "sha256-BTRI491/qgc8hw1IG/N7QB2fTDq6KIYuDOSpx9eKrL0=";
+    owner = "sdm845-mainline";
+    repo = "linux";
+    rev = "sdm845-6.16.7-r0";
+    hash = "sha256-XYlXuzapuesiTpvquuz0b6yPyAqEdK9lMdglST+EZhk=";
   };
-
-  patches = [
-    ./0003-qseecom-enable-oneplus6.patch
-  ];
 
   isModular = true;
   isCompressed = "gz";
